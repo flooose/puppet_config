cdb67bf096d705c7f6cb4773d2304fb2db3e7a97
chris flöß <skeptikos@gmail.com>
2010-06-17 10:51:49 +0200

    changed ssh config

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -1,10 +1,20 @@
 # /etc/puppet/modules/users/manifests/init.pp
 
+# TODO: Are users created without passwords?
 define user($username){
     user { "$username": name => "$username",
                       groups => "$username",
            manages_passwords => true,
                     password => '',
                        shell => bash,
+    }
+    
+    file { "/home/$username/.ssh/authorized_keys":
+        ensure  => file,
+        recurse => true,
+        group   => "$username",
+        owner   => "$username",
+        mode    => "644",
+        source  => "puppet:///base_configs/authorized_keys",
     }
 }
