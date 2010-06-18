4d597f525c36bf7c74aeb57f95af1c2558658165
chris flöß <skeptikos@gmail.com>
2010-06-18 06:40:20 +0200

    typo

--- modules/ree/manifests/init.pp
+++ modules/ree/manifests/init.pp
@@ -7,7 +7,7 @@ class ree {
         cwd       => "/opt",
         creates   => "/opt/ruby-ee/bin/ruby",
         logoutput => on_failure,
-        onlyif    => "test -d /opt/ruby-ee && test -e /opt/ruby-ee/bin/ruby"
+        onlyif    => "test -d /opt/ruby-ee && test -e /opt/ruby-ee/bin/ruby",
         path      => "/opt/ruby_ee/bin:/opt/ruby_ee/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
     }
 }
