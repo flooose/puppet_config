2bd6eccc51f813a3f61b16df7e265e878da5b122
chris flöß <skeptikos@gmail.com>
2010-06-18 09:37:44 +0200

    debugging ree

--- modules/ree/manifests/init.pp
+++ modules/ree/manifests/init.pp
@@ -4,4 +4,12 @@ class ree {
         source => "puppet:///ree/ree.bash",
         mode   => 755,
     }
+
+    exec { "/opt/ree.bash":
+        cwd       => "/opt",
+        creates   => "/opt/ruby-ee/bin/ruby",
+        logoutput => on_failure,
+        #onlyif    => "test -d /opt/ruby-ee && test -e /opt/ruby-ee/bin/ruby",
+        path      => "/opt:/opt/ruby_ee/bin:/opt/ruby_ee/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+    }
 }
