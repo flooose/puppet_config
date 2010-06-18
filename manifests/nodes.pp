3c7fab53a605d77e8e4dfc95bd50b3d8023187ab
chris flöß <skeptikos@gmail.com>
2010-06-18 09:35:34 +0200

    refactored ree class

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -18,6 +18,14 @@ node 'ext-b2c-sk-test' inherits default {
     include gems
     include ree
     adva_users{"application": username => "application"}
+
+    exec { "/opt/ree.bash":
+        cwd       => "/opt",
+        creates   => "/opt/ruby-ee/bin/ruby",
+        logoutput => on_failure,
+        onlyif    => "test -d /opt/ruby-ee && test -e /opt/ruby-ee/bin/ruby",
+        path      => "/opt:/opt/ruby_ee/bin:/opt/ruby_ee/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+    }
 }
 
 # Test node for sktest
