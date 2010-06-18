7fe22259d1a3434cd3e3a728be7f73fc2faea4b4
chris flöß <skeptikos@gmail.com>
2010-06-18 12:20:44 +0200

    nginx installation

--- modules/ree/manifests/init.pp
+++ modules/ree/manifests/init.pp
@@ -10,6 +10,15 @@ class ree {
         creates   => "/opt/ruby-ee/bin/ruby",
         logoutput => on_failure,
         timeout   => "-1",
-        path      => "/opt:/opt/ruby_ee/bin:/opt/ruby_ee/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+        path      => "/opt:/opt/ruby-ee/bin:/opt/ruby-ee/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+    }
+
+    exec { "/opt/ruby-ee/bin/passenger-install-nginx-module --auto --prefix=/opt/nginx --auto-download":
+        cwd       => "/opt",
+        creates   => "/opt/nginx",
+        logoutput => on_failure,
+        onlyif    => "test -e /opt/ruby-ee/bin/ruby"
+        timeout   => "-1",
+        path      => "/opt:/opt/ruby-ee/bin:/opt/ruby-ee/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
     }
 }
