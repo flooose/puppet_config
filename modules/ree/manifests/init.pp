b18b9e5f8bfc16d56784ddd1d386bc9874b164fc
chris flöß <skeptikos@gmail.com>
2010-06-18 13:55:46 +0200

    debugging

--- modules/ree/manifests/init.pp
+++ modules/ree/manifests/init.pp
@@ -14,10 +14,11 @@ class ree {
     }
 
     exec { "/opt/ruby-ee/bin/passenger-install-nginx-module --auto --prefix=/opt/nginx --auto-download":
+        require   => Exec["/opt/ree.bash"]
         cwd       => "/opt",
         creates   => "/opt/nginx",
         logoutput => on_failure,
-        #onlyif    => "test -e /opt/ruby-ee/bin/ruby",
+        onlyif    => "test -e /opt/ruby-ee/bin/ruby",
         timeout   => "-1",
         path      => "/opt:/opt/ruby-ee/bin:/opt/ruby-ee/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
     }
