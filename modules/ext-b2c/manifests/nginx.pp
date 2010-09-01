1492ed76a68c9208e4ccd2da9f86c34e21d65fa9
chris flöß <skeptikos@gmail.com>
2010-09-01 17:20:19 +0200

    Updated nginx config for and nginx manifest for new rvm-based system

--- modules/ext-b2c/manifests/nginx.pp
+++ modules/ext-b2c/manifests/nginx.pp
@@ -18,18 +18,18 @@ class ext-b2c::nginx {
             source  => "puppet:///ext-b2c/logrotate.d/nginx";
     }
 
-    exec { "/opt/ruby-ee/bin/passenger-install-nginx-module --auto --prefix=/opt/nginx --auto-download --extra-configure-flags='--with-http_ssl_module'":
-        require   => Exec["/opt/ree.bash"],
+    exec { "/home/application/.rvm/gems/ree-1.8.7-2010.02/bin/passenger-install-nginx-module --auto --prefix=/opt/nginx --auto-download --extra-configure-flags='--with-http_ssl_module'":
+        #require   => Exec["/opt/ree.bash"],
         cwd       => "/opt",
         creates   => "/opt/nginx",
         logoutput => true,
-        onlyif    => "test -e /opt/ruby-ee/bin/ruby",
+        onlyif    => "test -e /home/application/.rvm/rubies/ree-1.8.7-2010.02/bin/ruby",
         timeout   => "-1",
-        path      => "/opt:/opt/ruby-ee/bin:/opt/ruby-ee/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+        path      => "/home/application/.rvm/gems/ree-1.8.7-2010.02/bin:/home/application/.rvm/gems/ree-1.8.7-2010.02@global/bin:/home/application/.rvm/rubies/ree-1.8.7-2010.02/bin:/home/application/.rvm/bin:/opt/ruby-ee/bin:/opt/ruby-ee/sbin:/usr/local/bin:/usr/bin:/bin:/usr/games"
     }
 
     service { "nginx":
-        require    => Exec["/opt/ruby-ee/bin/passenger-install-nginx-module --auto --prefix=/opt/nginx --auto-download --extra-configure-flags='--with-http_ssl_module'"],
+        require    => Exec["/home/application/.rvm/gems/ree-1.8.7-2010.02/bin/passenger-install-nginx-module --auto --prefix=/opt/nginx --auto-download --extra-configure-flags='--with-http_ssl_module'"],
         #ensure     => running,
         enable     => true,
     }
@@ -37,24 +37,24 @@ class ext-b2c::nginx {
     file {
         # main nginx: /opt/nginx/nginx.conf
         "/opt/nginx/conf/nginx.conf":
-            require    => Exec["/opt/ruby-ee/bin/passenger-install-nginx-module --auto --prefix=/opt/nginx --auto-download --extra-configure-flags='--with-http_ssl_module'"],
-            source => "puppet:///ext-b2c/b2c-test/b2c-test.nginx.conf";
+            require     => Exec["/home/application/.rvm/gems/ree-1.8.7-2010.02/bin/passenger-install-nginx-module --auto --prefix=/opt/nginx --auto-download --extra-configure-flags='--with-http_ssl_module'"],
+            source      => "puppet:///ext-b2c/b2c-test/b2c-test.nginx.conf";
 
         # htaccess
         "/opt/nginx/conf/staging.sk.htpasswd":
-            source => "puppet:///ext-b2c/b2c-test/staging.sk.htpasswd";
+            source      => "puppet:///ext-b2c/b2c-test/staging.sk.htpasswd";
 
         # Sites available
         "/opt/nginx/conf/sites-available":
-            ensure => directory;
+            ensure      => directory;
         "/opt/nginx/conf/sites-available/ext.b2c.sk.test":
-            source => "puppet:///ext-b2c/b2c-test/ext.b2c.sk.test";
+            source      => "puppet:///ext-b2c/b2c-test/ext.b2c.sk.test";
 
         # Sites enabled
         "/opt/nginx/conf/sites-enabled":
-            ensure => directory;
+            ensure      => directory;
         "/opt/nginx/conf/sites-enabled/ext.b2c.sk.test":
-            ensure => "/opt/nginx/conf/sites-available/ext.b2c.sk.test",
-            require => File["/opt/nginx/conf/sites-available/ext.b2c.sk.test"];
+            ensure      => "/opt/nginx/conf/sites-available/ext.b2c.sk.test",
+            require     => kFile["/opt/nginx/conf/sites-available/ext.b2c.sk.test"];
     }
 }
